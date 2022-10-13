a0c07a8d0ab246e17702245e6e1e3a452b02d0d5
Jefrin-2212 <114570676+Jefrin-2212@users.noreply.github.com>
2022-10-13 15:08:55 +0530

    Array sorting

---
+++ Problems/problem2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class problem2 {
+    public static void main(String [] args){
+        int even=0;
+        int odd=0;
+
+        int evenCount=0;
+        int oddCount=1;
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        int arr1[] = new int[N];
+        for(int i=0;i<N;i++){
+            int a=sc.nextInt(); 
+            arr[i]=a;
+            if(a%2==0){
+                even+=1;
+            }
+            else{
+                odd+=1;
+            }
+        }    
+        sc.close();
+        if(even==odd){
+            System.out.println("Yes");
+            for(int j=0;j<N;j++){
+                if (arr[j]%2==0){
+                    arr1[evenCount]=arr[j];
+                }
+                else{
+                    arr[oddCount]=arr[j];
+                }
+            }
+            for(int k =0;k<N;k++){
+                System.out.print(arr1[k]+" ");
+            }
+        }
+        else{
+            System.out.println("No");
+        }   
+     
+}
+}
+   
